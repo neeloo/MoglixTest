68120a5768bdc6ccfe73ba3fe853ef88739f01c8
neeloo patel <nilusingh945164@gmail.com>
2026-07-21 21:20:35 +0530

    test

--- src/main/java/org/example/ValidParentheses.java
+++ src/main/java/org/example/ValidParentheses.java
@@ -1,4 +1,32 @@
 package org.example;
 
-public class ValidParanthesis {
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+
+        //String s = "(()";    ///output =2
+        String s =  ")()())";  /// output =4;
+        System.out.println(longestValidParentheses(s));
+
+    }
+    public  static int longestValidParentheses(String s) {
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+        int maxLength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                st.push(i);
+            } else {
+                st.pop();
+                if (st.isEmpty()) {
+                    st.push(i);
+                } else {
+                    int currLength = i-st.peek();
+                    maxLength = Math.max(maxLength, currLength);
+                }
+            }
+        }
+        return maxLength;
+    }
 }

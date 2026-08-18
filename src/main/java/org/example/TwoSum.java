35aada651129b5c611eed8bc3a7b5aa2bf873684
neeloo patel <nilusingh945164@gmail.com>
2026-08-18 16:11:55 +0530

    test

--- src/main/java/org/example/TwoSum.java
+++ src/main/java/org/example/TwoSum.java
@@ -4,7 +4,12 @@ public class TwoSum {
     public static void main(String[] args) {
 
         int arr[]= {2,3,4,5,6};
+        int target = 11;
+        TwoSumNumber(arr,target);
+    }
 
+    private static int TwoSumNumber(int[] arr, int target) {
+        return -1;
     }
 
 }

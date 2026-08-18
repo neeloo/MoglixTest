e4ae039f7a4c95650472a4d1fb4b5e83467880aa
neeloo patel <nilusingh945164@gmail.com>
2026-08-18 16:17:47 +0530

    test

--- src/main/java/org/example/TwoSum.java
+++ src/main/java/org/example/TwoSum.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
     public static void main(String[] args) {
 
@@ -8,8 +11,15 @@ public class TwoSum {
         TwoSumNumber(arr,target);
     }
 
-    private static int TwoSumNumber(int[] arr, int target) {
-        return -1;
+    private static int [] TwoSumNumber(int[] arr, int target) {
+        Map<Integer ,Integer>map =new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(target-arr[i])){
+                return new int[]{map.get(target-arr[i]),i};
+            }
+            map.put(arr[i],i);
+        }
+        return new int[]{-1,-1};
     }
 
 }
